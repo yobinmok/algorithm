98fa6ffcdf620aa894f970bf70559b9ad4c73434
yobinmok <dyqls1214@gmail.com>
2024-04-06 23:23:38 +0900

    add: swea 보급로

--- SWEA/SWEA_보급로.java
+++ SWEA/SWEA_보급로.java
@@ -3,19 +3,22 @@ package com.edu.week02;
 import java.io.*;
 import java.util.*;
 
+import SWEA_보급로1.Pos;
+
 public class SWEA_보급로 {
 
-	static int N;
+	static int N, min;
 	static int INF = Integer.MAX_VALUE;
 	static int[][] board, minDis;
 	static int[] dr = {0, 1, 0, -1}, dc = {1, 0, -1, 0};
 	static class Pos{
-		int r, c;
+		int r, c, dis;
 
-		public Pos(int r, int c) {
+		public Pos(int r, int c, int dis) {
 			super();
 			this.r = r;
 			this.c = c;
+			this.dis = dis;
 		}
 	}
 	
@@ -29,12 +32,6 @@ public class SWEA_보급로 {
 	 가중치 그래프 최단거리 -> 다익스트라(DP식)
 	 	- 다익스트라: 시작정점에서 다른 모든 정점으로의 최단경로를 구함!
 	 	- 프림/프루스칼(최소 신장 트리): 그래프 내의 모든 정점들을 가장 적은 비용으로 연결하기 위해 사용
-	 	
-	 "이동하는 시간에 비해 복구하는데 필요한 시간은 매우 크다고 가정한다.
-	 따라서, 출발지에서 도착지까지 거리에 대해서는 고려할 필요가 없다." ? 이건 뭐야
-	 
-	 가중치 그래프 최단거리라는 건 알았는데 어떤 알고리즘이었는지는 까먹어서 정리해둔거 참고함
-	 BFS, DFS는 뭐야 어케해
 	 * */
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("input.txt"));
@@ -65,52 +62,59 @@ public class SWEA_보급로 {
 //	3. 현재 위치에서 사방을 탐색해서 갈 수 있는 곳이라면 다음 위치의 dp값과 (현재 위치의 dp값 + 다음 위치 복구시간)을 비교해서 
 //	다음 위치의 dp값이 더 크다면 다음 위치를 deq에 담고 다음 위치의 dp값을 (현재 위치의 dp값 + 다음위치복구시간)으로 갱신시켜 준다.
 //	4. 그렇게 함수 실행이 끝나고 나면, dp의 도착지 위치에는 도착지까지 가는 최단 시간이 담기게 된다.
-//	https://january-diary.tistory.com/entry/SWEA-1249-%EB%B3%B4%EA%B8%89%EB%A1%9C-JAVA
 	static void dijkstra() {
-		// 1. 거리 배열 -> 무한대로 초기화 & (방문 배열도)
-		boolean[][] visited = new boolean[N][N];
 		minDis = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			Arrays.fill(minDis[i], INF);
 		}
-		
-		visited[0][0] = true;
 		minDis[0][0] = 0;
 		
-		Queue<Pos> que = new ArrayDeque<>();
-		que.add(new Pos(0, 0));
+		PriorityQueue<Pos> pq = new PriorityQueue<>((Pos p1, Pos p2) -> p1.dis - p2.dis);
+		pq.add(new Pos(0,0,0));
 		
-		while(!que.isEmpty()) {
-			Pos cur = que.poll();
-			for (int d = 0; d < 4; d++) {
+		while(!pq.isEmpty()) {
+			Pos cur = pq.poll();
+
+            if(cur.dis > minDis[cur.r][cur.c]) continue;
+			
+			for(int i=0; i< 4; i++) {
+				int nr = cur.r + dr[i];
+				int nc = cur.c + dc[i];
+				
+				if(nr <0 || nr >= N || nc < 0 || nc >= N) continue;
+				
+				//nr nc의 값이 현재 값보다 큰 경우만 탐색(갱신)
+				int nextTime = cur.dis + board[nr][nc];
+				if(minDis[nr][nc] > nextTime) {
+					minDis[nr][nc] = nextTime;
+					pq.add(new Pos(nr, nc, nextTime));
+				}
+			}
+		}
+	}
+	
+	static void bfs() {
+		boolean[][] visited = new boolean[N][N];
+		PriorityQueue<Pos> pq = new PriorityQueue<>((Pos p1, Pos p2) -> p1.dis - p2.dis);
+		pq.add(new Pos(0, 0, 0));
+		visited[0][0] = true;
+		
+		while(!pq.isEmpty()) {
+			Pos cur = pq.poll();
+			if(cur.r == N-1 && cur.c == N-1) {
+				min = cur.dis;
+				break;
+			}
+			
+			for(int d = 0; d < 4; d++) {
 				int nr = cur.r + dr[d];
 				int nc = cur.c + dc[d];
 				
-				if(nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc]) continue;
+				if(nr <0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc]) continue;
 				
 				visited[nr][nc] = true;
-				// 다음 위치의 dp값, 현재 위치의 dp값 + 다음 위치 시간 중 최소
-				minDis[nr][nc] = Math.min(minDis[nr][nc], minDis[cur.r][cur.c] + board[nr][nc]);
-				que.offer(new Pos(nr, nc));
+				pq.offer(new Pos(nr, nc, cur.dis + board[nr][nc]));
 			}
-		}
-//		for (int r = 0; r < N; r++) {
-//			for (int c = 0; c < N; c++) {
-//				for (int d = 0; d < 4; d++) {
-//					int nr = r + dr[d];
-//					int nc = c + dc[d];
-//					
-//					if(nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc]) continue;
-//					
-//					visited[nr][nc] = true;
-//					// 다음 위치의 dp값, 현재 위치의 dp값 + 다음 위치 시간 중 최소
-//					minDis[nr][nc] = Math.min(minDis[nr][nc], minDis[r][c] + board[nr][nc]);
-//				}
-//			}
-//		}
-		
-		for (int[] arr : minDis) {
-			System.out.println(Arrays.toString(arr));
 		}
 	}
 }
