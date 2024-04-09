c3d30c2060103169e254a07115d1780a76d28804
Mok Yobin <dyqls1214@gmail.com>
2024-04-09 17:55:23 +0900

    ing: boj 상어중학교 추가

--- BOJ/BOJ_상어중학교.java
+++ BOJ/BOJ_상어중학교.java
@@ -7,6 +7,18 @@ public class BOJ_상어중학교 {
 
 	static int N, M;
 	static int[][] board;
+	static int[] dr = {0, 1, 0, -1}, dc = {1, 0, -1, 0};
+	static class Block{
+		int r, c, size, rainbow;
+
+		public Block(int r, int c, int size, int rainbow) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.size = size;
+			this.rainbow = rainbow;
+		}
+	}
 	
 	/*
 	 블록 -> 검은색, 무지개, 일반
@@ -44,6 +56,64 @@ public class BOJ_상어중학교 {
 			}
 		}
 		
+		PriorityQueue<Block> pq  = new PriorityQueue<>((Block b1, Block b2) ->{
+			if(b2.size == b1.size) {
+				if(b2.rainbow == b1.rainbow) {
+					if(b2.r == b1.r) return b2.c - b1.c;
+					return b2.r - b1.r;
+				}
+				return b2.rainbow - b1.rainbow;
+			}
+			return b2.size - b1.size;
+		});
 		
+
+		while(true) {
+			// 1. 블록그룹 찾아 pq에 넣기 -> pq가 비어있으면 break;
+			// 0이 인접한 경우 그냥 다 포함해서 처리 -> 어차피 가장 큰 그룹 하나만 쓰니까
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if(board[i][j] == -1 || board[i][j] == -1) continue;
+					Block block = bfs(i, j);
+					if(block != null) pq.offer(block);
+				}
+			}
+			// 2. 가장 큰 블록 제거
+			Block big = pq.poll(); // 제거된 코드는 9로 해야징
+			
+			// 3. 중력 -> 반시계 -> 중력
+			// 중력 작용 시 검은 블록을 제외한 모든 블록이 아래로 이동, 다른 블록이나 경계를 만날때까지 이동
+		}
+	}
+	
+	static Block bfs(int r, int c) {
+		boolean[][] visited = new boolean[N][N];
+		Queue<int[]> que = new ArrayDeque<>();
+		que.add(new int[] {r, c});
+		visited[r][c] = true;
+
+		int size = 0, rainbow = 0;
+		while(!que.isEmpty()) {
+			int s = que.size();
+			for (int i = 0; i < s; i++) {
+				int[] cur = que.poll();
+				
+				for (int d = 0; d < 4; d++) {
+					int nr = cur[0] + dr[d];
+					int nc = cur[1] + dc[d];
+					
+					if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+					if(visited[nr][nc] || board[nr][nc] == -1) continue;
+					
+					if(board[nr][nc] == 0) rainbow += 1;
+					size += 1;
+					visited[nr][nc] = true;
+					que.offer(new int[] {nr, nc});
+				}
+			}
+		}
+		
+		if(size == 0) return null;
+		return new Block(r, c, size, rainbow);
 	}
 }
